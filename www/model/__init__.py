6aed5d3eac36e8eaa32d756781beaf47f85066e8
Nihar Sawant <nihar@3dtin.com>
2012-12-04 15:19:18 +0530

    Minor modifications in ConnectionString of mysql

--- www/model/__init__.py
+++ www/model/__init__.py
@@ -5,5 +5,5 @@ Base = declarative_base()
 Base.metadata = MetaData()
 
 def start_engine():
-  engine = create_engine('mysql://sampadm:secret@localhost/laterbox_db', echo=True)
+  engine = create_engine('mysql://sampadm:secret@localhost/laterbox_db', echo=False)
   Base.metadata.bind = engine
