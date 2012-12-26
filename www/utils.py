a695f69e20af0d21c671e55e601b541fd5779082
Nihar Sawant <nihar@3dtin.com>
2012-12-26 13:35:27 +0530

    separate file to store configuration data

--- www/utils.py
+++ www/utils.py
@@ -2,14 +2,18 @@
 import os
 import traceback
 import sys
+import ConfigParser
 
-SOURCE_DIR = os.path.join(os.environ['HOME'], 'git/laterbox')
-AWS_ACESS_KEY = 'AKIAJO6GMSYEBEBTGFQA'
-AWS_SECRET = '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'
-AWS_ARTICLE_DIR = 'articles/'
-BUCKET_NAME = 's3-apparatus'
+config = ConfigParser.ConfigParser()
+config.readfp(open('config.cfg', 'r'))
+
+SOURCE_DIR = os.path.join(os.environ['HOME'], config.get('ENV', 'src_dir'))
+AWS_ACESS_KEY = config.get('KEYS', 'aws')
+AWS_SECRET = config.get('SECRETS', 'aws')
+COOKIE_SECRET = config.get('SECRETS', 'cookie')
+AWS_ARTICLE_DIR = config.get('S3', 'article_dir')
+BUCKET_NAME = config.get('S3', 'bucket_name')
 DATETIME_FORMAT = '%m/%d/%Y %H:%M:%S'
-COOKIE_SECRET = 'd8f94e3118edd9a54304a07b900bae79868d2033c030d81a60d03ee117b329f7'
 
 def log_error():
   type_, value_, traceback_ = sys.exc_info()
