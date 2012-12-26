78a2d6f01dd12fdcbe12ef616c88ed896c15efdf
Nihar Sawant <nihar@3dtin.com>
2012-12-26 12:30:26 +0530

    renamed g.py to utils.py

--- www/server.py
+++ www/server.py
@@ -8,7 +8,7 @@ from lib.readability.readability import Document
 from sqlalchemy.orm import sessionmaker
 from lib.asyncs3 import AWSAuthConnection
 
-import g
+import utils
 from model import *
 from lib import shortner
 
@@ -22,7 +22,7 @@ class BaseHandler(web.RequestHandler):
     return reader
 
   def log_error(self, e):
-    g.log_error()
+    utils.log_error()
     self.error = e
     self.send_error()
 
@@ -38,7 +38,7 @@ class _404Handler(BaseHandler):
 class WelcomeHandler(BaseHandler):
   def get(self):
     try:
-      file_path = os.path.join(g.SOURCE_DIR, 'html/welcome.html')
+      file_path = os.path.join(utils.SOURCE_DIR, 'html/welcome.html')
       self.write(template.Template(open(file_path).read()).generate())
       self.finish()
     except Exception, e:
@@ -50,9 +50,9 @@ class WelcomeHandler(BaseHandler):
       password = self.get_argument('password', None)
       signup_type = self.get_argument('type', None)
 
-      if not email: raise g.AppException('empty_email')
-      if not password: raise g.AppException('empty_password')
-      if not signup_type: raise g.AppException('empty_type')
+      if not email: raise utils.AppException('empty_email')
+      if not password: raise utils.AppException('empty_password')
+      if not signup_type: raise utils.AppException('empty_type')
 
       session = sessionmaker(bind=Base.metadata.bind)()
 
@@ -60,7 +60,7 @@ class WelcomeHandler(BaseHandler):
 
       if signup_type == 'new':
         reader = session.query(Reader).filter(Reader.email == email).first()
-        if reader: raise g.AppException('duplicate_found')
+        if reader: raise utils.AppException('duplicate_found')
 
         password_hash = hashlib.sha512(password).hexdigest()
         reader = Reader(email, password_hash)
@@ -73,12 +73,12 @@ class WelcomeHandler(BaseHandler):
         reader = session.query(Reader).filter(Reader.email == email).first()
         password_hash = hashlib.sha512(password).hexdigest()
 
-        if reader is None: raise g.AppException('invalid_email')
-        if reader.password_hash != password_hash: raise g.AppException('invalid_password')
+        if reader is None: raise utils.AppException('invalid_email')
+        if reader.password_hash != password_hash: raise utils.AppException('invalid_password')
 
         self.set_secure_cookie('__user__', str(reader.id))
         self.redirect('/')
-      else: raise g.AppException('invalid_type')
+      else: raise utils.AppException('invalid_type')
 
     except Exception, e:
       self.log_error(e)
@@ -95,7 +95,7 @@ class MainHandler(BaseHandler):
     try:
       reader = self.get_logged_user()
       if reader:
-        file_path = os.path.join(g.SOURCE_DIR, 'html/index.html')
+        file_path = os.path.join(utils.SOURCE_DIR, 'html/index.html')
         self.write(template.Template(open(file_path).read()).generate())
         self.finish()
       else:
@@ -115,7 +115,7 @@ class ReadingListHandler(BaseHandler):
           url = article.url,
           title = article.title,
           description = article.description,
-          created_tstamp = article.created_tstamp.strftime(g.DATETIME_FORMAT)
+          created_tstamp = article.created_tstamp.strftime(utils.DATETIME_FORMAT)
         ))
 
       self.write(json.dumps(article_list))
@@ -129,14 +129,14 @@ class ArticleHandler(BaseHandler):
   def s3_download_complete(self, response):
     try:
       if response.error:
-        raise g.AppException('s3_download_failed')
+        raise utils.AppException('s3_download_failed')
       params = dict(
         id = self.uid,
         url = self.article.url,
         body = response.body,
         title = self.article.title,
         description = self.article.description,
-        created_tstamp = self.article.created_tstamp.strftime(g.DATETIME_FORMAT)
+        created_tstamp = self.article.created_tstamp.strftime(utils.DATETIME_FORMAT)
       )
 
       self.write(json.dumps(params))
@@ -155,8 +155,8 @@ class ArticleHandler(BaseHandler):
       article_id = shortner.to_decimal(self.uid)
       self.article = self.session.query(Page).filter_by(id=article_id).one()
 
-      aws = AWSAuthConnection(g.AWS_ACESS_KEY, g.AWS_SECRET, is_secure=False)
-      aws.get(g.BUCKET_NAME, g.AWS_ARTICLE_DIR+self.article.md5_hash,
+      aws = AWSAuthConnection(utils.AWS_ACESS_KEY, utils.AWS_SECRET, is_secure=False)
+      aws.get(utils.BUCKET_NAME, utils.AWS_ARTICLE_DIR+self.article.md5_hash,
         callback=self.s3_download_complete
       )
     except Exception, e:
@@ -186,10 +186,10 @@ class AddHandler(BaseHandler):
 
       exists = self.session.query(Page).filter_by(md5_hash=self.article_md5).count()
       if exists :
-        raise g.AppException('article_already_exists')
+        raise utils.AppException('article_already_exists')
 
-      aws = AWSAuthConnection(g.AWS_ACESS_KEY, g.AWS_SECRET, is_secure=False)
-      aws.put(g.BUCKET_NAME, g.AWS_ARTICLE_DIR+self.article_md5, self.article_body,
+      aws = AWSAuthConnection(utils.AWS_ACESS_KEY, utils.AWS_SECRET, is_secure=False)
+      aws.put(utils.BUCKET_NAME, utils.AWS_ARTICLE_DIR+self.article_md5, self.article_body,
         {'Content-Type' : 'text/html', 'x-amz-acl' : 'public-read'},
         callback=self.s3_upload_complete
       )
@@ -205,7 +205,7 @@ class AddHandler(BaseHandler):
       url = self.get_argument('url', None)
       exists = self.session.query(Page).filter_by(url=url).count()
       if exists :
-        raise g.AppException('article_already_exists')
+        raise utils.AppException('article_already_exists')
 
       doc = urllib.urlopen(url)
       html = doc.read()
@@ -213,7 +213,7 @@ class AddHandler(BaseHandler):
       self.url = doc.geturl()
       exists = self.session.query(Page).filter_by(url=self.url).count()
       if exists :
-        raise g.AppException('article_already_exists')
+        raise utils.AppException('article_already_exists')
 
       self.article_body = Document(html).summary().encode('utf-8')
       self.article_title = Document(html).short_title()
@@ -227,7 +227,7 @@ class AddHandler(BaseHandler):
 
 settings = dict(
   debug=True,
-  cookie_secret=g.COOKIE_SECRET
+  cookie_secret=utils.COOKIE_SECRET
 )
 
 handler_list = [
@@ -238,9 +238,9 @@ handler_list = [
   ('/welcome', WelcomeHandler),
   ('/logout', LogoutHandler),
 
-  ('/js/(.*)', web.StaticFileHandler, {'path' : os.path.join(g.SOURCE_DIR, 'js')}),
-  ('/css/(.*)', web.StaticFileHandler, {'path' : os.path.join(g.SOURCE_DIR, 'css')}),
-  ('/img/(.*)', web.StaticFileHandler, {'path' : os.path.join(g.SOURCE_DIR, 'img')}),
+  ('/js/(.*)', web.StaticFileHandler, {'path' : os.path.join(utils.SOURCE_DIR, 'js')}),
+  ('/css/(.*)', web.StaticFileHandler, {'path' : os.path.join(utils.SOURCE_DIR, 'css')}),
+  ('/img/(.*)', web.StaticFileHandler, {'path' : os.path.join(utils.SOURCE_DIR, 'img')}),
 
   ('/(.*)', _404Handler)
 ]
